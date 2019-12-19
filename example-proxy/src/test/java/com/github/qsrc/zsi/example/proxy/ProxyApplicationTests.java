8485fc48d001b4d57872b74c3c2f464a40110d1f
Adrian Pauli <pauli.adrian96@gmail.com>
2019-12-19 22:28:21 +0100

    Initialize project and example projects

---
+++ example-proxy/src/test/java/com/github/qsrc/zsi/example/proxy/ProxyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.qsrc.zsi.example.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
