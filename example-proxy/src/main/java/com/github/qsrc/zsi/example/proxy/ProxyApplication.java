c2c5528ff91de3aa87d6ea4fe41a05bb79f9ce4c
Adrian Pauli <pauli.adrian96@gmail.com>
2019-12-21 23:53:22 +0100

    Add proxy configuration

--- example-proxy/src/main/java/com/github/qsrc/zsi/example/proxy/ProxyApplication.java
+++ example-proxy/src/main/java/com/github/qsrc/zsi/example/proxy/ProxyApplication.java
@@ -2,7 +2,9 @@ package com.github.qsrc.zsi.example.proxy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableZuulProxy
 @SpringBootApplication
 public class ProxyApplication {
 
